ae148fb53575a34be1ac339c4349e810c2f68ce1
Tangsuluu <karybekovatansuluu303@gmail.com>
2024-08-09 14:47:05 +0600

    new component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { RouterProvider } from "react-router-dom";
+import { router } from './router';
 
 function App() {
   return (
     <div>
-    
+      <RouterProvider router={router}/>
     </div>
   )
 }
