02740a4c0cf9443d8916d813410638a94b922829
Tangsuluu <karybekovatansuluu303@gmail.com>
2024-09-18 16:40:19 +0600

    check git

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function About() {
   return (
     <div>
-      About
+      About Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, eos aperiam dolorum labore voluptatem hic. Vel sit eius laborum totam iusto nulla quae corrupti, itaque maiores animi consequatur dolores optio saepe esse facere ut placeat? Voluptate nisi ea labore, perspiciatis tempore nulla eaque sit non natus aspernatur suscipit rem nemo nam fuga cum modi qui eligendi quaerat praesentium aliquam in eum quam odio? Dolores magni tempore quod, aperiam qui nostrum veniam, doloribus repellat fugiat assumenda similique adipisci dolorem! Impedit consequuntur quam quisquam, tempore explicabo fugit molestiae corporis saepe! Eius, facilis error, repellendus officiis ratione quo illo cum vitae at voluptatum commodi, animi asperiores minima ex. Velit, reprehenderit. Alias, numquam. Corporis debitis fugiat vel nam inventore adipisci possimus, nisi ex omnis quibusdam consequatur unde reiciendis quis ab eaque vero esse facilis voluptates eius dignissimos porro, ipsam quas minima consectetur. Nobis mollitia facilis aspernatur praesentium molestias qui rerum cum animi amet laborum est, aliquid consequuntur odio voluptates assumenda saepe, sint sapiente minus explicabo quas! Temporibus dolore modi quasi illo provident tenetur enim beatae eum a necessitatibus ab, inventore incidunt unde laudantium dignissimos in aspernatur amet perferendis similique voluptates recusandae. Est impedit dolores nobis sequi quam magni. Tempora non at blanditiis quas eius.
     </div>
   )
 }
